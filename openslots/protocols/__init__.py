75d8f5234b1110803f500167565a58a6c7d82147
Kopachris <kopachris@gmail.com>
2016-02-14 02:30:45 -0700

    Updated package structure

---
+++ openslots/protocols/__init__.py
@@ -0,0 +1,1 @@
+"""Communication protocols for peripherals and accounting systems"""
---
+++ openslots/protocols/sas.py
@@ -0,0 +1,12 @@
+"""Implement IGT's Slot Accounting System protocol"""
+
+
+___SAS_version___ = 602     # SAS version 6.02
+
+
+class SASGame(object):
+    pass
+
+
+class SASHost(object):
+    pass
