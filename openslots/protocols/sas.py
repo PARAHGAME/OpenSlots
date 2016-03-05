3bf4194cf58a637e85663953046f1e7e7521d8a6
Kopachris <kopachris@gmail.com>
2016-03-05 14:03:33 -0700

    Working on ideas for SASByte

--- openslots/protocols/sas.py
+++ openslots/protocols/sas.py
@@ -1,6 +1,9 @@
 """Implement IGT's Slot Accounting System protocol"""
 
 
+from collections import namedtuple, Counter
+
+
 ___SAS_version___ = 602     # SAS version 6.02
 
 
@@ -14,20 +17,29 @@ def crc(b, seed=0):
     return (seed).to_bytes(2, byteorder='little')
 
 
+SASByte = namedtuple('SASByte', ['x', 'addr'])
+
+
 class SASGame(object):
-    def SE_validation_number(self, v_id, v_seq):
+    def __init__(self):
+        self._meters = Counter()
+        self._v_id = 0
+        self._v_seq = 0
+
+    def SE_validation_number(self):
         """Generate secure-enhanced ticket validation number from seed
         values. Returns string representing the 18-digit validation
         number.
         """
         
-        if 0 > v_id >= 2**24:
-            raise ValueError("Validation ID too large, %i" % v_id)
-        if 0 > v_seq >= 2**24:
-            raise ValueError("Validation sequence too large, %i" % v_seq)
+        # TODO move to v_id and v_seq assignment from host
+        # if 0 > self._v_id >= 2**24:
+        #    raise ValueError("Validation ID too large, %i" % self._v_id)
+        # if 0 > self._v_seq >= 2**24:
+        #    raise ValueError("Validation sequence too large, %i" % self._v_seq)
         
-        a = [x for x in (v_seq).to_bytes(3, byteorder='little')]
-        a += [x for x in (v_id).to_bytes(3, byteorder='little')]
+        a = [x for x in self._v_seq.to_bytes(3, byteorder='little')]
+        a += [x for x in self._v_id.to_bytes(3, byteorder='little')]
         
         b = [0] * 6
         b[5] = a[5] ^ a[1]
