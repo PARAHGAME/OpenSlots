f9c8236ee2e969c3fbe4ad6918cf18e715739650
Chris Koch <kopachris@gmail.com>
2016-03-15 01:42:02 -0600

    Added chi-squared test

--- openslots/utils.py
+++ openslots/utils.py
@@ -36,5 +36,43 @@ class RNG(object):
 
     def choice(self, seq):
         """Return a random item from a given sequence"""
+
         r = math.floor(self._cur * len(seq))
         return seq[r]
+
+    def chisquare(self, k=1000, n=1000000):
+        """Perform a chi-square goodness of fit test on the RNG.
+
+        Args:
+            k: Number of evenly-spaced ranges to categorize samples into.
+            n: Number of samples.
+
+        Returns:
+            Pearson's cumulative test statistic, X^2, calculated as the sum of:
+            observations in a category minus expected observations in that
+            category, squared, divided by the expected observations in that
+            category, for every category.
+
+            You'll want to compare the resulting number against a chi-squared
+            distribution table for the required p-value and k-1 degrees of
+            freedom.
+        """
+
+        if self.is_cycling:
+            self.stop_cycle()
+            self.is_cycling = True
+
+        U = [(i/k, (i+1)/k) for i in range(k)]
+        N = [self._rng.random() for i in range(n)]
+        x = 0
+        for i in U:
+            u = 0
+            for j in N:
+                if i[0] < j <=i[1]:
+                    u += 1
+            x += (u - (n/k))**2 / (n/k)
+
+        if self.is_cycling:
+            self.cycle()
+
+        return x
